23a51ed54f042ca27d930c56a2afe78e23d07844
Ant0C <cafeo97@gmail.com>
2023-02-28 16:37:28 +0100

    FEAT exercise

---
+++ my-vue-AppTitle/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  data() {
+    return {
+      message: 'La mia prima app con Vite!'
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="bg-blu">
+    <h1>
+      {{ message }}
+    </h1>
+  </div>
+</template>
+
+<style>
+.bg-blu {
+  background-color: blue;
+  color: white;
+}
+</style>
